07b1081f263e60fb73776a6263a2e55e7e2fb9db
Robert Hjalmers <robert.hjalmers@seb.se>
2018-08-07 14:39:28 +0200

    ci(travis): updated travis deploy config
    added github pages deploy step and release deploys are now restricted to master branch

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $TRAVIS_PULL_REQUEST
 echo $TRAVIS_BRANCH
-if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "develop" ];
+if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "master" ];
     then
         echo "Publishing package to npm";
         cd dist/lib
