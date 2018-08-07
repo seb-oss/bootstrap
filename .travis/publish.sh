3b6232a5c75d3c2ff2f408ddcf25f400065a093b
Robert Hjalmers <robert.hjalmers@gmail.com>
2018-08-07 13:42:30 +0200

    updated publish script

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "development" ]; then
-    echo "Publishing package to npm";
-    cd dist/lib
-    _auth = $NPM_TOKEN > ~/.npmrc
-    email = $NPM_EMAIL >> ~/.npmrc
-    npm publish
-    echo "Package successfully published to npm";
+echo $TRAVIS_PULL_REQUEST
+echo $TRAVIS_BRANCH
+if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "development" ];
+    then
+        echo "Publishing package to npm";
+        cd dist/lib
+        _auth = $NPM_TOKEN > ~/.npmrc
+        email = $NPM_EMAIL >> ~/.npmrc
+        npm publish
+        echo "Package successfully published to npm"
     else
-    echo "Publishing package to npm failed";
+        echo "Publishing package to npm failed"
     exit 1
 fi
