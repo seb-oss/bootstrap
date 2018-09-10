97bd452bf504204bca9fa09f4e60358d3f12eaf5
Robert Hjalmers <robert.hjalmers@seb.se>
2018-09-10 17:12:31 +0200

    docs(buttons): update example

--- src/example/components/buttons/buttons.component.ts
+++ src/example/components/buttons/buttons.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as testHtml from '!raw-loader!./test.html';
-import { of } from 'rxjs';
 
 @Component({
   selector: 'app-buttons',
@@ -8,30 +6,6 @@ import { of } from 'rxjs';
   styleUrls: ['./buttons.component.scss']
 })
 export class ButtonsComponent implements OnInit {
-
-  SNIPPETS = {
-    main: [
-      {
-        name: 'markup',
-        lang: 'markup',
-        src: 'buttons/buttons-main/buttons-main.component.html'
-      },{
-        lang: 'typescript',
-        src: 'buttons/buttons-main/buttons-main.component.ts'
-      },
-      {
-        lang: 'typescript',
-        src: 'https://raw.githubusercontent.com/ng-bootstrap/ng-bootstrap/master/demo/src/app/components/buttons/demos/checkbox/buttons-checkbox.ts'
-      }
-    ],
-    sizes: [
-      {
-        name: 'markup',
-        lang: 'markup',
-        src: 'buttons/buttons-sizes/buttons-sizes.component.html'
-      }
-    ]
-  };
 
   constructor() { }
 
--- src/example/example.module.ts
+++ src/example/example.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ButtonsComponent} from './components/buttons/buttons.component';
 import { CodeComponent } from './shared/components/code/code.component';
-import {ButtonsMainComponent} from './components/buttons/buttons-main/buttons-main.component';
 import { SnippetWrapperComponent } from './shared/components/snippet-wrapper/snippet-wrapper.component';
 import {HttpClientModule} from '@angular/common/http';
-import { ButtonsSizesComponent } from './components/buttons/buttons-sizes/buttons-sizes.component';
 import { AlertsComponent } from './components/alerts/alerts.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import {FormsModule} from '@angular/forms';
@@ -48,9 +46,7 @@ library.add(faArrowAltRight);
   declarations: [
       ButtonsComponent,
       CodeComponent,
-      ButtonsMainComponent,
       SnippetWrapperComponent,
-      ButtonsSizesComponent,
       AlertsComponent,
       TabsComponent,
       TablesComponent,
