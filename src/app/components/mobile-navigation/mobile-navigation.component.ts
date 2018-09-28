11ec0279c6e8dc61fba8e2b2a0728f567ca0b9aa
Robert Hjalmers <robert.hjalmers@gmail.com>
2018-09-28 13:59:52 +0200

    docs: improve mobile navigation

--- src/app/components/mobile-navigation/mobile-navigation.component.ts
+++ src/app/components/mobile-navigation/mobile-navigation.component.ts
@@ -11,7 +11,7 @@ import {Observable, Subject} from 'rxjs';
   animations: [
     trigger('slideOut', [
       transition(':leave',
-        animate('500ms ease-out',
+        animate('300ms ease-out',
           style({
             opacity: 0,
             transform: 'translateX(-100px)'
@@ -23,7 +23,7 @@ import {Observable, Subject} from 'rxjs';
           opacity: 0,
           transform: 'translateX(-100px)'
         }),
-        animate('700ms ease-in',
+        animate('500ms ease-in',
           style({
             opacity: 1,
             transform: 'translateX(0)'
@@ -33,7 +33,7 @@ import {Observable, Subject} from 'rxjs';
     ]),
     trigger('slideIn', [
       transition(':leave',
-        animate('700ms ease-out',
+        animate('300ms ease-out',
           style({
             opacity: 0,
             transform: 'translateX(700px)'
@@ -43,7 +43,7 @@ import {Observable, Subject} from 'rxjs';
           opacity: 0,
           transform: 'translateX(700px)'
         }),
-        animate('700ms ease-in',
+        animate('500ms ease-in',
           style({
             opacity: 1,
             transform: 'translateX(0)'
@@ -72,37 +72,19 @@ export class MobileNavigationComponent implements OnInit {
 
   $activeState: Subject<Array<MenuItem>> = new Subject<Array<MenuItem>>();
   $activeTab: Observable<string>;
-  $activeMobile: Subject<boolean> = new Subject<boolean>();
 
   constructor(private menuService: MenuService) { }
   public components:Array<MenuItem> = [];
   menuState: string = 'show';
-  menu2State: string = 'hide';
 
   ngOnInit() {
     this.$activeTab = this.menuService.$activeTab;
-    this.$activeTab.subscribe(res => this.loadMenu(res));
+    this.$activeState = this.menuService.$menuItems
 
-  }
-
-  navigate(nextState: any, currentState: any) {
-  }
-
-  loadMenu(tab?: string) {
-    if (tab === 'components') {
-      this.$activeState = this.menuService.$menuItems;
-      //this.$activeState = this.menuService.$menuItems;
-    }
   }
 
   hideMobileNav() {
     this.menuService.switchTab(null);
   }
 
-  animationDone($event) {
-    //this.$activeState.next(state);
-    //this.menuState = 'show';
-    //this.menu2State = 'hide';
-    console.log($event);
-  }
 }
