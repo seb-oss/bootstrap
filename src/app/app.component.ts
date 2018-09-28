247ab7501cc586f54770f30ee23bfbb8cdefcf0a
Robert Hjalmers <robert.hjalmers@gmail.com>
2018-09-28 21:10:41 +0200

    docs: improve navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { environment } from '../environments/environment';
 export class AppComponent {
   scrollPosition: number;
   travis_build_number: string = environment.travis_build_number;
-  version: string = environment.version === '0.0.0-semantically-released' ? 'latest' : 'tag/v' + environment.version;
+  version: string = (environment.version === '0.0.0-semantically-released' || environment.version === 'n/a') ? 'latest' : 'tag/v' + environment.version;
 
   scroll($event) {
     this.scrollPosition = $event.target.scrollingElement.scrollTop;
