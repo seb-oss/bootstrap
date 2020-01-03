3c6a4e25dbe7e49efc7b215c25ce5d64a08606d2
Robert Hjalmers <robert.hjalmers@gmail.com>
2020-01-03 20:32:03 +0100

    docs(button): add import instructions and close btn

--- src/example/components/buttons/buttons.component.ts
+++ src/example/components/buttons/buttons.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ScssImport} from '../../../app/interfaces/scss-import';
 
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
-  styleUrls: ['./buttons.component.scss']
+  styleUrls: ['./buttons.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ButtonsComponent implements OnInit {
+export class ButtonsComponent {
 
+  components: Array<ScssImport> = [{
+    title: 'buttons',
+    fileName: 'button',
+    comment: 'buttons'
+  },{
+    title: 'close button',
+    fileName: 'close',
+    comment: 'close button'
+  }];
   constructor() { }
-
-  ngOnInit() {
-  }
 
 
 }
